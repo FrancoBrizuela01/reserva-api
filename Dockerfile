aac04e30a6a1f61b2dbb34a4c56c27232725540b
FrancoBrizuela01 <franco-alexis16@hotmail.com>
2025-02-28 11:02:42 -0300

    Cambiar versión a .NET 8 para compatibilidad con Railway

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+﻿# Usa la imagen de .NET 8
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+COPY ["ReservaApi.csproj", "./"]
+RUN dotnet restore "./ReservaApi.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "ReservaApi.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "ReservaApi.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "ReservaApi.dll"]
--- obj/Debug/net9.0/ReservaApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ReservaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0745d057c7906e64f971713192b20677c76c0762")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
